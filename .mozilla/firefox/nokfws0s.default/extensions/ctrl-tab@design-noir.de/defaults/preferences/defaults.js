4c5d674ee221739fd72a45e9656c984b0ef039e4
root <root@Mansarovar.(none)>
2009-01-05 17:00:53 -0800

    Removing unwanted files and directories

--- .mozilla/firefox/nokfws0s.default/extensions/ctrl-tab@design-noir.de/defaults/preferences/defaults.js
+++
@@ -1,4 +0,0 @@
-pref("browser.ctrlTab.mostRecentlyUsed", true);
-pref("browser.ctrlTab.recentlyUsedLimit", 7);
-pref("browser.ctrlTab.smoothScroll", true);
-pref("browser.allTabs.allWindows", true);
--- .mozilla/firefox/nokfws0s.default/extensions/fastdial@telega.phpnet.us/defaults/preferences/fastdial.js
+++
@@ -1,3 +0,0 @@
-pref("extensions.fastdial@telega.phpnet.us.description", "chrome://fastdial/locale/fastdial.properties");
-pref("extensions.fastdial.showInNewTabs", true);
-pref("extensions.fastdial.showEmpty", true);
